d1e0b17f7bdb08c1e0f9cc981ed3f6cbcfd26597
Conor McCarthy <conor.mccarthy@tillerinvest.com>
2021-02-18 11:49:57 +0000

    p37-1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function MyThing() {
+  return (
+    <div className='book'>
+      <div className='title'>
+        The Title
+      </div>
+      <div className='author'>
+        The Author
+      </div>
+      <ul className='stats'>
+        <li className='rating'>
+          5 stars
+        </li>
+        <li className='isbn'>
+          12-345678-910
+        </li>
+      </ul>
+
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <MyThing/>,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
